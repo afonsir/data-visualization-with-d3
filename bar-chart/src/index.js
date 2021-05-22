fb574b81f35b02ac5168a0212538269c6c8f54a4
AfonSir <afonsir17@gmail.com>
2021-05-21 21:02:32 -0300

    feat: add x axis title

--- bar-chart/src/index.js
+++ bar-chart/src/index.js
@@ -17,7 +17,7 @@ const height = +svg.attr('height')
 const render = data => {
   const xValue = d => d.population
   const yValue = d => d.country
-  const margin = { top: 50, right: 40, bottom: 20, left: 100 }
+  const margin = { top: 50, right: 40, bottom: 50, left: 100 }
   const innerWidth = width - margin.left - margin.right
   const innerHeight = height - margin.top - margin.bottom
 
@@ -45,10 +45,16 @@ const render = data => {
     .selectAll('.domain, .tick line')
       .remove()
 
-  g.append('g').call(xAxis)
+  const xAxisG = g.append('g').call(xAxis)
     .attr('transform', `translate(0, ${innerHeight})`)
-    .select('.domain')
-      .remove()
+
+  xAxisG.select('.domain').remove()
+
+  xAxisG.append('text')
+    .attr('y', 45)
+    .attr('x', innerWidth / 2)
+    .attr('fill', 'black')
+    .text('Population')
 
   g.selectAll('rect').data(data)
     .enter().append('rect')
